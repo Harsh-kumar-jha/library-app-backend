deb9a62644ad7fa8d0f6a1e4a09989bceadc0736
Harsh Kumar Jha <jhaharshkumar293@gmail.com>
2024-08-16 21:29:36 +0530

    Added security and Extracted Jwt token generated by okta to fetch user email

--- src/main/java/com/backend/restapi/spring_boot_library/controller/BookController.java
+++ src/main/java/com/backend/restapi/spring_boot_library/controller/BookController.java
@@ -2,6 +2,7 @@ package com.backend.restapi.spring_boot_library.controller;
 
 import com.backend.restapi.spring_boot_library.entity.Book;
 import com.backend.restapi.spring_boot_library.service.BookService;
+import com.backend.restapi.spring_boot_library.utils.ExtractJWT;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin("http://localhost:3000")
@@ -16,20 +17,20 @@ public class BookController {
     }
 
     @GetMapping("/secure/currentLoans/count")
-    public int currentLoan(){
-        String userEmail =  "testuser@email.com";
+    public int currentLoan(@RequestHeader(value = "Authorization") String token){
+        String userEmail = ExtractJWT.payloadJWTExtraction(token,"\"sub\"");
         return bookService.currentLoanCount(userEmail);
     }
 
     @GetMapping("/secure/isCheckedOut/byUser")
-    public Boolean checkoutBookByUser(@RequestParam Long bookId){
-        String userEmail = "testuser@email.com";
+    public Boolean checkoutBookByUser(@RequestHeader(value = "Authorization") String token,@RequestParam Long bookId){
+        String userEmail = ExtractJWT.payloadJWTExtraction(token,"\"sub\"");
         return bookService.checkoutBookByUser(userEmail,bookId);
     }
 
     @PostMapping("/secure/checkout")
-    public Book checkoutBook(@RequestParam Long bookId)throws Exception{
-        String userEmail = "testuser@email.com";
+    public Book checkoutBook(@RequestHeader(value = "Authorization") String token,@RequestParam Long bookId)throws Exception{
+        String userEmail = ExtractJWT.payloadJWTExtraction(token,"\"sub\"");
         return bookService.checkoutBook(userEmail,bookId);
     }
 }
